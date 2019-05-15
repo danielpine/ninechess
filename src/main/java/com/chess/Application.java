3e1c3f95db7b6bda93371c2dfe04b05c24a33a45
danielpine <danielpine@sina.com>
2019-05-15 21:32:27 +0800

    init

---
+++ src/main/java/com/chess/Application.java
@@ -0,0 +1,18 @@
+package com.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description ：
+ * @author :Daniel Pine
+ * @email :danielpine@sina.com
+ * @date :2019年5月15日-下午5:23:07
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
+}
---
+++ src/test/java/com/chess/ChessBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChessBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
